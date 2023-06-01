7013417f9e212f41be41c38a616261dff38cdbdb
vystii <ngahabanda@gmail.com>
2023-06-01 14:50:44 +0100

    creation

---
+++ src/Controller/chatAppController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\example\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * main chat_app controller.
+ */
+class chatAppController extends ControllerBase
+{
+
+    /**
+     * Returns a render-able array for a test page.
+     */
+    public function index()
+    {
+        $build = [
+            '#markup' => $this->t('Hello World!'),
+        ];
+        return $build;
+    }
+}
